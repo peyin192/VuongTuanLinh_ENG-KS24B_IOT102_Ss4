0d1b3fd9aeda7ba6a56c4bdc214a0e516663ef99
peyin192 <vuonglinh190206.qv@gmail.com>
2025-06-27 13:06:47 +0700

    Add files via upload

--- PTIT_CNTT2_IT201_Ss05_Ex3/ex3.c
+++ PTIT_CNTT2_IT201_Ss05_Ex3/ex3.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
-int main() {
-    void factoria(int n) {
-        if (n > 0) {
 
-        }
+int calculateFactorial(int n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * calculateFactorial(n - 1);
     }
+}
+
+int main() {
+    int positiveInteger;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &positiveInteger);
+
+    if (positiveInteger < 0) {
+        printf("Invalid input\n");
+    } else {
+        int result = calculateFactorial(positiveInteger);
+        printf("%d\n", result);
+    }
+
+    return 0;
 }
